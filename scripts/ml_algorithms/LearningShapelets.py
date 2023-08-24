11d4fa216e3781ff5f4f8aa00e13a070cd5bdb05
Julio_Riveros <jcr.cardus.2020@aluno.unila.edu.br>
2023-08-24 12:07:51 -0300

    added smoothing

--- scripts/ml_algorithms/LearningShapelets.py
+++ scripts/ml_algorithms/LearningShapelets.py
@@ -2,14 +2,15 @@ import os
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.metrics import accuracy_score
 from tslearn.preprocessing import TimeSeriesScalerMinMax
 from tslearn.shapelets import LearningShapelets, grabocka_params_to_shapelet_size_dict
 from tslearn.utils import ts_size
 
 # Carregar seus dados
-folder_dementia = r"C:\Users\Lenovo\Desktop\IC\[99] Database Final (16_287)\cookie_d"
-folder_control = r"C:\Users\Lenovo\Desktop\IC\[99] Database Final (16_287)\cookie_c/"
+folder_dementia = r"C:\Users\Lenovo\Desktop\IC\[99] Database Final (16_4296)\cookie_d"
+folder_control = r"C:\Users\Lenovo\Desktop\IC\[99] Database Final (16_4296)\cookie_c/"
 
 data_dementia = []
 for npy_file in os.listdir(folder_dementia):
@@ -26,18 +27,30 @@ for npy_file in os.listdir(folder_control):
 X = np.concatenate((data_dementia, data_control), axis=0)
 y = np.concatenate((np.ones(len(data_dementia)), np.zeros(len(data_control))), axis=0)
 
+# Definir o tamanho da janela da média móvel
+window_size = 100
+
+# Aplicar a média móvel para todas as linhas
+X_smooth = np.zeros_like(X)  # Inicializar uma matriz para armazenar os resultados suavizados
+
+for i in range(X.shape[0]):
+    serie = pd.Series(X[i])
+    rolling = serie.rolling(window=window_size)
+    serie_smooth = rolling.mean().to_numpy()
+    X_smooth[i] = serie_smooth # Agora X_smooth contém as séries temporais suavizadas
+
 # Normalize each of the timeseries
-X = TimeSeriesScalerMinMax().fit_transform(X)
+X_smooth = TimeSeriesScalerMinMax().fit_transform(X)
 
 # Get statistics of the dataset
-n_ts, ts_sz = X.shape[:2]
+n_ts, ts_sz = X_smooth.shape[:2]
 n_classes = len(set(y))
 
 # Set the number of shapelets per size as done in the original paper
 shapelet_sizes = grabocka_params_to_shapelet_size_dict(n_ts=n_ts,
                                                        ts_sz=ts_sz,
                                                        n_classes=n_classes,
-                                                       l=0.8,
+                                                       l=0.01, #0.8
                                                        r=1)
 
 # Define the model using parameters provided by the authors (except that we
@@ -46,13 +59,13 @@ shp_clf = LearningShapelets(n_shapelets_per_size=shapelet_sizes,
                             optimizer=tf.optimizers.Adam(.01),
                             batch_size=549,
                             weight_regularizer=.01,
-                            max_iter=600,
-                            random_state=42,
+                            max_iter=300,
+                            random_state=1,
                             verbose=0)
-shp_clf.fit(X, y)
+shp_clf.fit(X_smooth, y)
 
 # Make predictions and calculate accuracy score
-pred_labels = shp_clf.predict(X)
+pred_labels = shp_clf.predict(X_smooth)
 print("Correct classification rate:", accuracy_score(y, pred_labels))
 
 # Plot the different discovered shapelets
